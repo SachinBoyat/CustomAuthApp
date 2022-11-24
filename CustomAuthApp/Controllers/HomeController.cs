3b641613a1ee68f15c2375534c627ae10ffb5908
Sachin Boyat <scboyat90@gmail.com>
2022-11-24 09:52:18 +0530

    first commit

---
+++ CustomAuthApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CustomAuthApp.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomAuthApp.Controllers
+{
+   [ CustomAuthenticationFilter]
+    public class HomeController : Controller
+    {
+        [CustomAuthorize("Normal", "SuperAdmin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [CustomAuthorize("Admin", "SuperAdmin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [CustomAuthorize("SuperAdmin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
